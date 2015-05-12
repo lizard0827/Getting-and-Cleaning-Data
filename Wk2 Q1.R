d2c9a4c9a0161f6466ce3750545a5361a7117636
lizard0827 <lizard0827@gmail.com>
2015-05-12 00:07:47 -0500

    Update Wk2 Q1.R

--- Wk2 Q1.R
+++ Wk2 Q1.R
@@ -1,8 +1,13 @@
-setwd("/Users/lizard0827/Desktop/class/Getting and Cleaning data/")
-fileurl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
-destfile<-"acs.csv"
-download.file(fileurl, destfile, method="curl")
-library(sqldf)
-acs<-read.csv("acs.csv")
-head(acs)
-sqldf("select pwgtp1 from acs where AGEP < 50")
+library(httr)
+#Find oauth setting for github
+oauth_endpoints("github")
+#register with my key and secret
+myapp=oauth_app("Github", key="46f88530ef6c6c3cb0c5", 
+                secret="ac38811eba6c11327e14fc471fb6d751a6a43eb5")
+# Get oauth credentials
+github_token<-oauth2.0_token(oauth_endpoints("github"), myapp)
+#Use API
+gtoken<-config(token=github_token)
+req<-GET("https://api.github.com/users/jtleek/repos", gtoken)
+stop_for_status(req)
+content<-content(req)
